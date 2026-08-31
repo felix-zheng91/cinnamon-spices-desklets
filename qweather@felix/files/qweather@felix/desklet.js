a74de87c85aa1ba224ac387711186925dfc9baa5
Felix <felix.zheng91@outlook.com>
2026-08-31 11:26:32 +0800

    qweather@felix: Restore license and icon attribution header

--- qweather@felix/files/qweather@felix/desklet.js
+++ qweather@felix/files/qweather@felix/desklet.js
@@ -9,6 +9,22 @@
  * Based on bbcwx@oak-wood.co.uk by Chris Hastie (GPLv3), itself forked
  * from accudesk@logan by loganj. Original code Copyright 2013 loganj,
  * 2014-2018 Chris Hastie, 2026 felix.
+ *
+ * Icons: QWeather Icons (c) QWeather, CC BY 4.0, see icons/qweather/;
+ * additional icon sets from bbcwx, see icons/ subdirectories.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 const ByteArray = imports.byteArray;
